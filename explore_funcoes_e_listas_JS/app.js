c63a2d91f3361879ee7489062badd6e6531645ab
Monica Carvalho Rossi <monicacrrossi@gmail.com>
2024-09-05 10:12:30 -0300

    Adicionar projeto Jogo do Número Secreto

---
+++ explore_funcoes_e_listas_JS/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do númeor secreto';
+
+let parafrafo = document.querySelector('p');
+parafrafo.innerHTML = 'Escolha do númeor entre 1 e 10';
+
+function verificarChute() {
+    
+}
