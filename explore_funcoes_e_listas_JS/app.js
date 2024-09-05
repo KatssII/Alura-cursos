7250eb204b135627f28cb461e7c2635806886597
Monica Carvalho Rossi <monicacrrossi@gmail.com>
2024-09-05 14:23:04 -0300

    Alteração verificando o chute no jogo

--- explore_funcoes_e_listas_JS/app.js
+++ explore_funcoes_e_listas_JS/app.js
@@ -16,7 +16,17 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabéns! Você acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!!');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior!');
+        }
+    }
 }
 
 function gerarNumeroAleatorio() {
